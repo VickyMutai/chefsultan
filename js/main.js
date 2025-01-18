c772bd44b23d9d742ee425b9c219845065f991f2
Victoria Mutai <vicky.mutai96@gmail.com>
2025-01-18 19:57:43 +0300

    Update animation for the pages

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 
-        document.addEventListener('DOMContentLoaded', function() {
-            const navbar = document.querySelector('.navbar');
-            const heroHeight = document.querySelector('.hero').offsetHeight;
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.querySelector('.navbar');
+    const heroHeight = document.querySelector('.hero').offsetHeight;
 
-            window.addEventListener('scroll', function() {
-                if (window.scrollY > heroHeight) {
-                    navbar.classList.add('bg-custom', 'text-white');
-                    navbar.classList.remove('bg-light');
-                } else {
-                    navbar.classList.remove('bg-custom', 'text-white');
-                    navbar.classList.add('bg-light');
-                }
-            });
-        });
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > heroHeight) {
+            navbar.classList.add('bg-custom', 'text-white');
+            navbar.classList.remove('bg-light');
+        } else {
+            navbar.classList.remove('bg-custom', 'text-white');
+            navbar.classList.add('bg-light');
+        }
+    });
+});
+// Scroll Animation
+document.addEventListener("scroll", function () {
+    const elements = document.querySelectorAll(".fade-on-scroll");
+    elements.forEach(element => {
+      const rect = element.getBoundingClientRect();
+      if (rect.top < window.innerHeight && rect.bottom >= 0) {
+        element.classList.add("fade-in");
+      }
+    });
+  });
+  
